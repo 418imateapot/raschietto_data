b39d428f024cff86b834aabaef786c39419eacc8
savdema <demarco.sa@gmail.com>
2015-11-26 11:58:05 +0100

    Update oneQueryToRuleThemAll.py

--- oneQueryToRuleThemAll.py
+++ oneQueryToRuleThemAll.py
@@ -14,21 +14,27 @@ query = """
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     #www.dlib.org/dlib/november14/beel/11beel_ver1
 
-    SELECT ?id ?author ?title ?year
+    SELECT ?id ?author ?title ?year ?comment ?url
+   ?comment ?url
     WHERE {
       ?x raschietto:type "hasAuthor";
         oa:hasBody ?y.
       ?y rdf:object ?z.
       ?z rdfs:label ?author.
       ?y rdf:subject ?id.
-
       ?x1 raschietto:type "hasTitle";
         oa:hasBody ?y1.
       ?y1 rdf:object ?title.
-      ?x2 raschietto:type "hasPublicationYear";
+ 	  ?x2 raschietto:type "hasPublicationYear";
   		oa:hasBody ?y2.
   	  ?y2 rdf:object ?year.
-    }
+  	  ?x3 raschietto:type "hasComment";
+  		oa:hasBody ?y3.
+  	  ?y3 rdf:object ?comment.
+  	   ?x4 raschietto:type "hasURL";
+  		oa:hasBody ?y4.
+  	  ?y4 rdf:object ?url.
+  	  }
     LIMIT 100
 """
 
